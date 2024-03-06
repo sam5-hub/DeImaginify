086eac2edbe5d07a7233154c962527c234901081
SUNG5 <sungwong555@gmail.com>
2024-03-06 22:04:32 +0800

    username fix

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -64,6 +64,7 @@ export async function POST(req: Request) {
     const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
+      username: first_name,
       firstName: first_name,
       lastName: last_name,
       photo: image_url,
@@ -90,6 +91,7 @@ export async function POST(req: Request) {
     const user = {
       firstName: first_name,
       lastName: last_name,
+      username: first_name!,
       photo: image_url,
     };
 
